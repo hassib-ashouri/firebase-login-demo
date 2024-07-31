cd8045b89b5883fda4528a7c9977fdcc201dbcef
Hassib Ashouri <hassib291@hotmail.com>
2024-07-30 19:45:04 -0700

    Use a new firebase project

--- src/firebase/client.js
+++ src/firebase/client.js
@@ -1,12 +1,11 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDnS_dhRQM8BKYoCPVy0VX8AQmkkYU2lTg",
-    authDomain: "lendnow-login.firebaseapp.com",
-    projectId: "lendnow-login",
-    storageBucket: "lendnow-login.appspot.com",
-    messagingSenderId: "1038614721846",
-    appId: "1:1038614721846:web:46f87024b3774e56116f97"
+  apiKey: "AIzaSyANJnHO9wbN_O7GjakJb-RiRJDn-x5BiYk",
+  authDomain: "fir-demo-dev-e8e07.firebaseapp.com",
+  projectId: "fir-demo-dev-e8e07",
+  storageBucket: "fir-demo-dev-e8e07.appspot.com",
+  messagingSenderId: "515220316582",
+  appId: "1:515220316582:web:5074f307667d8a7be42f67"
 };
-
 export const app = initializeApp(firebaseConfig);
